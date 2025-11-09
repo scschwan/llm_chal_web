c7c996ff6e0354c7870b5032c18101b2c09f15ae
Taekhyeon Nam <bigth@Taekhyeons-MacBook-Pro.local>
2025-11-09 15:02:57 +0900

    Init

---
+++ src/main/java/kr/co/dimillion/lcapp/LcAppApplication.java
@@ -0,0 +1,13 @@
+package kr.co.dimillion.lcapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LcAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LcAppApplication.class, args);
+    }
+
+}
