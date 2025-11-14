a480a9d940ca888eb0505119a723882ac7f16192
bigth21 <taekhyeon.nam@sofit.kr>
2025-11-14 16:13:49 +0900

    Configure default user by role

---
+++ src/main/java/kr/co/dimillion/lcapp/infrastructure/SecurityConfig.java
@@ -0,0 +1,49 @@
+package kr.co.dimillion.lcapp.infrastructure;
+
+import kr.co.dimillion.lcapp.application.Role;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration(proxyBeanMethods = false)
+public class SecurityConfig {
+    @Bean
+    UserDetailsService userDetailsService() {
+        UserDetails worker = User.withDefaultPasswordEncoder()
+                .username("worker")
+                .password("1234")
+                .roles(Role.WORKER.name())
+                .build();
+
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("1234")
+                .roles(Role.ADMIN.name())
+                .build();
+
+        return new InMemoryUserDetailsManager(worker, admin);
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().authenticated())
+                .formLogin(Customizer.withDefaults());
+
+        return http.build();
+    }
+}
