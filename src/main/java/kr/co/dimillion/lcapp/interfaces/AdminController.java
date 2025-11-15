a02e2ef8b7e72b42daacaee9da5603372c0acd2e
bigth21 <taekhyeon.nam@sofit.kr>
2025-11-15 18:10:30 +0900

    Implement products inquiry

--- src/main/java/kr/co/dimillion/lcapp/interfaces/AdminController.java
+++ src/main/java/kr/co/dimillion/lcapp/interfaces/AdminController.java
@@ -1,15 +1,25 @@
 package kr.co.dimillion.lcapp.interfaces;
 
+import kr.co.dimillion.lcapp.application.Product;
+import kr.co.dimillion.lcapp.application.ProductRepository;
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Controller
 @RequestMapping("/admin")
+@RequiredArgsConstructor
 public class AdminController {
+    private final ProductRepository productRepository;
+
     @GetMapping
     public String admin() {
         return "admin";
@@ -21,7 +31,12 @@ public class AdminController {
     }
 
     @GetMapping("/product-management")
-    public String productManagement(@ModelAttribute ProductCreateForm productCreateForm) {
+    public String productManagement(@ModelAttribute ProductCreateForm productCreateForm, Model model) {
+        List<ProductDto> productDtoList = productRepository.findAll()
+                .stream()
+                .map(ProductDto::from)
+                .toList();
+        model.addAttribute("products", productDtoList);
         return "product-management";
     }
 
@@ -36,5 +51,11 @@ public class AdminController {
         private String code;
         private String name;
         private String description;
+    }
+
+    public record ProductDto(Integer id, String code, String name, LocalDateTime createdAt, boolean active) {
+        public static ProductDto from(Product product) {
+            return new ProductDto(product.getId(), product.getCode(), product.getName(), product.getCreatedAt(), product.isActive());
+        }
     }
 }
