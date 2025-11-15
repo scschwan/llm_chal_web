2a7a41c06702bbe43695c10f60453dc9a8f7fec1
bigth21 <taekhyeon.nam@sofit.kr>
2025-11-15 17:10:04 +0900

    Markup product-management create section

--- src/main/java/kr/co/dimillion/lcapp/interfaces/AdminController.java
+++ src/main/java/kr/co/dimillion/lcapp/interfaces/AdminController.java
@@ -1,7 +1,10 @@
 package kr.co.dimillion.lcapp.interfaces;
 
+import lombok.Data;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -18,7 +21,20 @@ public class AdminController {
     }
 
     @GetMapping("/product-management")
-    public String productManagement() {
+    public String productManagement(@ModelAttribute ProductCreateForm productCreateForm) {
         return "product-management";
+    }
+
+    @PostMapping("/product-management/product")
+    public String product(@ModelAttribute ProductCreateForm productCreateForm) {
+        System.out.println("productCreateForm = " + productCreateForm);
+        return "redirect:/admin/product-management";
+    }
+
+    @Data
+    public static class ProductCreateForm {
+        private String code;
+        private String name;
+        private String description;
     }
 }
