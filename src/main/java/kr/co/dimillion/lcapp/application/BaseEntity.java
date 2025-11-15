bffd21d5e4fce60173be04a6dba88f284fdbd603
bigth21 <taekhyeon.nam@sofit.kr>
2025-11-15 17:59:54 +0900

    Create product entity

---
+++ src/main/java/kr/co/dimillion/lcapp/application/BaseEntity.java
@@ -0,0 +1,18 @@
+package kr.co.dimillion.lcapp.application;
+
+import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+}
