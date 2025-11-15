3b7e2e7a3d49de3f2e241ddf63177f401439868b
bigth21 <taekhyeon.nam@sofit.kr>
2025-11-15 12:42:40 +0900

    Configure AccessDeniedHandler

--- src/main/java/kr/co/dimillion/lcapp/infrastructure/security/SecurityConfig.java
+++ src/main/java/kr/co/dimillion/lcapp/infrastructure/security/SecurityConfig.java
@@ -1,9 +1,13 @@
 package kr.co.dimillion.lcapp.infrastructure.security;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import kr.co.dimillion.lcapp.application.Role;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -12,6 +16,9 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+import java.io.IOException;
 
 @Configuration(proxyBeanMethods = false)
 public class SecurityConfig {
@@ -48,7 +55,11 @@ public class SecurityConfig {
                 .formLogin(login -> login
                         .loginPage("/login").permitAll()
                         .loginProcessingUrl("/login/proc")
-                        .successHandler(new RoleBasedAuthenticationSuccessHandler()));
+                        .successHandler(new RoleBasedAuthenticationSuccessHandler()))
+                .exceptionHandling(exception -> exception
+                        .accessDeniedHandler((AccessDeniedHandler) (request, response, accessDeniedException) -> {
+                            response.sendRedirect("/login?denied");
+                        }));
         return http.build();
     }
 }
