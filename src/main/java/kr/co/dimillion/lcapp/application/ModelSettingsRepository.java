d2edbeef05245930162e09f0a7cf99adbb876867
bigth21 <taekhyeon.nam@sofit.kr>
2025-11-16 21:45:50 +0900

    Create model settings repository

---
+++ src/main/java/kr/co/dimillion/lcapp/application/ModelSettingsRepository.java
@@ -0,0 +1,6 @@
+package kr.co.dimillion.lcapp.application;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ModelSettingsRepository extends JpaRepository<ModelSettings, Integer> {
+}
